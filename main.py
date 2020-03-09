e858c14d82e9897466fa645c4922d234f4900618
MstfH <mo.mh43@gmail.com>
2020-03-09 11:26:55 +0100

    module stream video with motion detection and mqtt

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import communicationModule.communicationModule as mtq
 import time
-import logging
+#import logging
 import asyncio
 
 mtqq = mtq.MqttCommunicator()
 
 if __name__ == '__main__':
     formatter = "[%(asctime)s] %(name)s {%(filename)s:%(lineno)d} %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.DEBUG, format=formatter)
+    #logging.basicConfig(level=logging.DEBUG, format=formatter)
     # calling send function in communicationModule.py
-    asyncio.get_event_loop().run_until_complete(mtqq.send('sensor_input'))
+    asyncio.get_event_loop().run_until_complete(mtqq.send('#RIP'))
+    #find ipv4 address on arch with command 'ip route show' use the one after 'dhcp src'
+    mtq.camStream('145.137.65.63',8000)
