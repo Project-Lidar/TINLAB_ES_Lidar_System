31aa96260b0e5a0c5f19bd77d2856c7bd59dfc9b
Froztbyt3 <Jul_daniel@hotmail.com>
2020-03-19 14:40:19 +0100

    Subscribed to new mqtt topics en added comments

--- controllerModule/controllerModule.py
+++ controllerModule/controllerModule.py
@@ -6,23 +6,28 @@ from hbmqtt.mqtt.constants import QOS_1, QOS_2
 
 class MqttCommunicator:
     def __init__(self):
-        self.C = MQTTClient('Comm_module')  # Initialize the mqtt client
+        self.C = MQTTClient('Controller_module')  # Initialize the mqtt client
 
     @asyncio.coroutine
     def getController(self):
         # connect to mqtt broker
         yield from self.C.connect('mqtt://eecfbf0c:59ea275059b9c893@broker.shiftr.io/')
         yield from self.C.subscribe([
-            ('controls/manual/controller/', QOS_2),
+            # Subscribed to speed topic
+            ('controls/manual/controller/speed', QOS_2),
+            # Subscribed to steer topic
+            ('controls/manual/controller/steer', QOS_2),
+            # Subscribed to driving topic
             ('controls/driving/', QOS_2),
         ])
         try:
+            # Wait for 100 inputs from the broker before disconnecting and unsubscribing from the broker (for test purposes)
             for i in range(1, 100):
                 message = yield from self.C.deliver_message()
                 packet = message.publish_packet
                 print("%d:  %s => %s" % (
                     i, packet.variable_header.topic_name, str(packet.payload.data)))
-            yield from self.C.unsubscribe(['controls/manual/controller/', 'contols/driving/'])
+            yield from self.C.unsubscribe(['controls/manual/controller/speed', 'controls/manual/controller/steer', 'contols/driving/'])
             yield from self.C.disconnect()
         except ClientException as ce:
             logger.error("Client exception: %s" % ce)
