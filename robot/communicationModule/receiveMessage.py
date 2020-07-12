bb12992c60bb73022186adbf3f3ffcc1005d417c
MstfH <mo.mh43@gmail.com>
2020-07-12 18:28:19 +0200

    Update receiveMessage.py

--- robot/communicationModule/receiveMessage.py
+++ robot/communicationModule/receiveMessage.py
@@ -1,5 +1,6 @@
 import robot.motorControl.manualLogic as manual
 
+#decide where message should go
 def messageLogic(topic, message):
     if topic == 'contols/driving/':
         manual.driveState(message)
